54c314e54b2d735279fd89663b31462be8064529
justinkay <justinkay92@gmail.com>
2023-09-13 11:33:54 -0700

    Added initial CFC configs

--- datasets.py
+++ datasets.py
@@ -1,9 +1,20 @@
 from detectron2.data.datasets import register_coco_instances
 
-# Cityscapes -> Foggy Cityscapes
+# Cityscapes 
 register_coco_instances("cityscapes_train", {},         "datasets/cityscapes/annotations/cityscapes_train_instances.json",                  "cityscapes/")
 register_coco_instances("cityscapes_val",   {},         "datasets/cityscapes/annotations/cityscapes_val_instances.json",                    "cityscapes/")
+
+# Foggy Cityscapes
 register_coco_instances("cityscapes_foggy_train", {},   "datasets/cityscapes_foggy/annotations/cityscapes_train_instances_foggyALL.json",   "cityscapes_foggy/")
 register_coco_instances("cityscapes_foggy_val", {},     "datasets/cityscapes_foggy/annotations/cityscapes_val_instances_foggyALL.json",     "cityscapes_foggy/")
 # for evaluating COCO-pretrained models: category IDs are remapped to match
 register_coco_instances("cityscapes_foggy_val_coco_ids", {},     "datasets/cityscapes_foggy/annotations/cityscapes_val_instances_foggyALL_coco.json",     "cityscapes_foggy/")
+
+# Sim10k
+# TODO
+
+# CFC
+register_coco_instances("cfc_train", {},         "datasets/cfc/coco_labels/cfc_train.json",                  "datasets/cfc/images/cfc_train/")
+register_coco_instances("cfc_val",   {},         "datasets/cfc/coco_labels/cfc_val.json",                    "datasets/cfc/images/cfc_val/")
+# register_coco_instances("cfc_channel_train", {},         "datasets/cfc/coco_labels/cfc_channel_train.json",                  "datasets/cfc/")
+register_coco_instances("cfc_channel_test",   {},         "datasets/cfc/coco_labels/cfc_channel_test.json",                    "datasets/cfc/images/cfc_channel_test/")
