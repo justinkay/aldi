921ccb4187a49a4121bac21872bf985e9c330b8b
justinkay <justinkay92@gmail.com>
2023-09-15 07:40:41 -0700

    Update cityscapes paths

--- datasets.py
+++ datasets.py
@@ -5,16 +5,18 @@ register_coco_instances("cityscapes_train", {},         "datasets/cityscapes/ann
 register_coco_instances("cityscapes_val",   {},         "datasets/cityscapes/annotations/cityscapes_val_instances.json",                    "datasets/cityscapes/leftImg8bit/val/")
 
 # Foggy Cityscapes
-register_coco_instances("cityscapes_foggy_train", {},   "datasets/cityscapes_foggy/annotations/cityscapes_train_instances_foggyALL.json",   "datasets/cityscapes_foggy/leftImg8bit/train/")
-register_coco_instances("cityscapes_foggy_val", {},     "datasets/cityscapes_foggy/annotations/cityscapes_val_instances_foggyALL.json",     "datasets/cityscapes_foggy/leftImg8bit/val/")
+register_coco_instances("cityscapes_foggy_train", {},   "datasets/cityscapes/annotations/cityscapes_train_instances_foggyALL.json",   "datasets/cityscapes/leftImg8bit_foggy/train/")
+register_coco_instances("cityscapes_foggy_val", {},     "datasets/cityscapes/annotations/cityscapes_val_instances_foggyALL.json",     "datasets/cityscapes/leftImg8bit_foggy/val/")
 # for evaluating COCO-pretrained models: category IDs are remapped to match
-register_coco_instances("cityscapes_foggy_val_coco_ids", {},     "datasets/cityscapes_foggy/annotations/cityscapes_val_instances_foggyALL_coco.json",     "datasets/cityscapes_foggy/leftImg8bit/val/")
+register_coco_instances("cityscapes_foggy_val_coco_ids", {},     "datasets/cityscapes/annotations/cityscapes_val_instances_foggyALL_coco.json",     "datasets/cityscapes/leftImg8bit_foggy/val/")
 
 # Sim10k
-# TODO
+register_coco_instances("sim10k_cars_train", {},             "datasets/sim10k/coco_car_annotations.json",                  "datasets/sim10k/images/")
+register_coco_instances("cityscapes_cars_train", {},         "datasets/cityscapes/annotations/cityscapes_train_instances_cars.json",                  "datasets/cityscapes/leftImg8bit/train/")
+register_coco_instances("cityscapes_cars_val",   {},         "datasets/cityscapes/annotations/cityscapes_val_instances_cars.json",                    "datasets/cityscapes/leftImg8bit/val/")
 
 # CFC
 register_coco_instances("cfc_train", {},         "datasets/cfc/coco_labels/cfc_train.json",                  "datasets/cfc/images/cfc_train/")
 register_coco_instances("cfc_val",   {},         "datasets/cfc/coco_labels/cfc_val.json",                    "datasets/cfc/images/cfc_val/")
-# register_coco_instances("cfc_channel_train", {},         "datasets/cfc/coco_labels/cfc_channel_train.json",                  "datasets/cfc/images/cfc_channel_train/a")
+# register_coco_instances("cfc_channel_train", {},         "datasets/cfc/coco_labels/cfc_channel_train.json",                  "datasets/cfc/images/cfc_channel_train/")
 register_coco_instances("cfc_channel_test",   {},         "datasets/cfc/coco_labels/cfc_channel_test.json",                    "datasets/cfc/images/cfc_channel_test/")
