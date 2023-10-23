5ee6dd87d82f6a7a21c393d3c90f7206085962a9
Timm Haucke <timm@haucke.xyz>
2023-10-23 14:51:36 -0400

    cleanup slurm training script

--- train_net_slurm.sh
+++ train_net_slurm.sh
@@ -1,23 +1,39 @@
 #!/bin/bash
-#SBATCH --job-name=daod-strong-baseline-test  # short name for the job
-#SBATCH --partition=xeon-g6-volta        # partition to run on
-#SBATCH --exclusive
-#SBATCH --ntasks=4                       # total task count
-#SBATCH --nodes=4                        # node count
-#SBATCH --ntasks-per-node=1              # total number of tasks per node
-#SBATCH --cpus-per-task=40               # cpu-cores per task (>1 if multi-threaded tasks)
-#SBATCH --mem=40G
-#SBATCH --gres=gpu:volta:2               # number of gpus per node
-#SBATCH --mail-type=begin                # send email when job begins
-#SBATCH --mail-type=end                  # send email when job ends
-#SBATCH --mail-user=kayj@mit.edu
 
-export MASTER_PORT=$(expr 10000 + $(echo -n $SLURM_JOBID | tail -c 4))
-export WORLD_SIZE=$(($SLURM_NNODES * $SLURM_NTASKS_PER_NODE))
+#SBATCH --job-name=daod-strong-baseline       # short name for the job
+#SBATCH --partition=xeon-g6-volta             # partition to run on
+#SBATCH --exclusive                           # use entire node
+#SBATCH --ntasks=4                            # total task count
+#SBATCH --nodes=4                             # node count
+#SBATCH --ntasks-per-node=1                   # total number of tasks per node
+#SBATCH --cpus-per-task=40                    # cpu-cores per task (>1 if multi-threaded tasks)
+#SBATCH --mem=40G                             # memory
+#SBATCH --gres=gpu:volta:2                    # type and number of GPUs per node
+#SBATCH --output=slurm/slurm-%j.out           # slurm output files
+
+# setup distributed training
+export LEADER_PORT=$(expr 10000 + $(echo -n $SLURM_JOBID | tail -c 4))
+export WORLD_SIZE=$(($SLURM_JOB_NUM_NODES * $SLURM_NTASKS_PER_NODE))
+export LEADER_ADDR=$(scontrol show hostnames "$SLURM_JOB_NODELIST" | head -n 1)
 echo "WORLD_SIZE="$WORLD_SIZE
+echo "LEADER_ADDR="$LEADER_ADDR
 
-master_addr=$(scontrol show hostnames "$SLURM_JOB_NODELIST" | head -n 1)
-export MASTER_ADDR=$master_addr
-echo "MASTER_ADDR="$MASTER_ADDR
+# use both GPUs
+export CUDA_VISIBLE_DEVICES="0,1"
 
-srun -N4 ./train_net_slurm_instance.sh "$@"
+# configuration values
+CFG="${1:-configs/cityscapes/cityscapes_baseline/Base-RCNN-FPN-Cityscapes.yaml}"
+
+# setup SuperCloud module system
+module purge
+module load anaconda
+
+# run training script inside anaconda environment
+srun -N$SLURM_JOB_NUM_NODES bash -c "\
+    conda run --no-capture-output -n daod-strong-baseline \
+        python train_net.py \
+            --machine-rank \$SLURM_PROCID \
+            --num-machines $SLURM_JOB_NUM_NODES \
+            --num-gpus 2 \
+            --dist-url tcp://$LEADER_ADDR:$LEADER_PORT \
+            --config-file $CFG ${@:2}"
