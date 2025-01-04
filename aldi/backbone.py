eb7dffc9f2119c14c60945712f7b6b8cea80404a
justinkay <justinkay92@gmail.com>
2025-01-04 16:58:17 -0500

    Add Vit-L code.

--- aldi/backbone.py
+++ aldi/backbone.py
@@ -43,7 +43,29 @@ def build_vitdet_b_backbone(cfg, input_shape):
     backbone.net.forward = partial(checkpointed_vit_forward, backbone.net, cfg.VIT.USE_ACT_CHECKPOINT)
     return backbone
 
-def get_adamw_optim(model, params={}, include_vit_lr_decay=False):
+@BACKBONE_REGISTRY.register()
+def build_vitdet_l_backbone(cfg, input_shape):
+    backbone = model_zoo.get_config("common/models/mask_rcnn_vitdet.py").model.backbone
+    backbone.square_pad = 0 # disable square padding
+
+    # ViT-L stuff
+    backbone.net.embed_dim = 1024
+    backbone.net.depth = 24
+    backbone.net.num_heads = 16
+    backbone.net.drop_path_rate = 0.4
+    # 5, 11, 17, 23 for global attention
+    backbone.net.window_block_indexes = (
+        list(range(0, 5)) + list(range(6, 11)) + list(range(12, 17)) + list(range(18, 23))
+    )
+
+    backbone = instantiate(backbone)
+
+    # TODO doesn't work yet
+    # backbone.net.forward = partial(checkpointed_vit_forward, backbone.net, cfg.VIT.USE_ACT_CHECKPOINT)
+   
+    return backbone
+
+def get_adamw_optim(model, params={}, include_vit_lr_decay=False, vit_size='b'):
     """See detectron2/projects/ViTDet/configs/COCO/mask_rcnn_vitdet_b_100ep.py"""
     optimizer = model_zoo.get_config("common/optim.py").AdamW
     # From VitDet paper: We also use a layer-wise lr decay [10][2] of 0.7/0.8/0.9 for ViT-B/L/H with 
@@ -51,7 +73,12 @@ def get_adamw_optim(model, params={}, include_vit_lr_decay=False):
     # hierarchical backbones or ViT with supervised pre-training.**
     # Thus, disabling the following line by default, since we pretrain with COCO.
     if include_vit_lr_decay:
-        optimizer.params.lr_factor_func = partial(get_vit_lr_decay_rate, num_layers=12, lr_decay_rate=0.7)
+        if vit_size == 'b':
+            optimizer.params.lr_factor_func = partial(get_vit_lr_decay_rate, num_layers=12, lr_decay_rate=0.7)
+        elif vit_size == 'l':
+            optimizer.params.lr_factor_func = partial(get_vit_lr_decay_rate, lr_decay_rate=0.8, num_layers=24)
+        else:
+            raise ValueError(f"ViT size {vit_size} not supported.")
     optimizer.params.overrides = { "pos_embed": {"weight_decay": 0.0}, }
     for p in params:
         setattr(optimizer, p, params[p])
