9d0335ad838fee2a1e7503262a69932a66b91da6
dangerstone <stinnadanger@gmail.com>
2025-03-03 13:50:00 +0100

    merged all annotations by location

--- scripts/COCO_merge.py
+++ scripts/COCO_merge.py
@@ -37,7 +37,7 @@ def merge_coco_json(coco_list):
 
         # Print progress every 2 files
         if index % 2 == 0 or index == total_files:
-            print(f"Processed {index} out of {total_files} files")
+            print(f"--- Processed {index} out of {total_files} files.")
 
     merged = {}
     merged["info"] = merged_info
@@ -47,35 +47,40 @@ def merge_coco_json(coco_list):
     merged["categories"] = merged_categories
 
     return merged
-         
 
 def save_merged_annotations(merged, destination_path):
     # Save the merged dataset
     with open(destination_path, "w") as f:
         json.dump(merged, f, indent=4)
 
+def merge_by_location(src_dir="data-annotations/pitfall-cameras/originals-converted/", dest_dir= "data-annotations/pitfall-cameras/merged-by-location/"):
+    # Create lists of paths to all files to merge for each location
+    files = [f for f in os.listdir(src_dir) if os.path.isfile(os.path.join(src_dir, f)) and f.endswith(".json")]
+    location_to_file_list = {}
+    for loc in ccu.LOCATIONS:
+        location_to_file_list[loc] = []
+    for f in files:
+        # if file pertains to this location, add its path to the respective list
+        file_prefix = "_".join(f.split("_")[0:3])
+        if file_prefix not in location_to_file_list.keys(): raise ValueError(f"Location does not exist for file, {f}")
+        location_to_file_list[file_prefix].append(os.path.join(src_dir, f))
+    print(location_to_file_list)
+        
+    for loc in ccu.LOCATIONS:
+        merged = merge_coco_json(location_to_file_list[loc])
+        save_merged_annotations(merged=merged, destination_path=dest_dir+loc+".json")
 
-def merge_by_location(field, crop, camera, src_dir="data-annotations/pitfall-cameras/originals-converted/"):
-    # Find the files pertaining to the given field, crop 
-    prefix = "_".join([field, crop, camera])
-    files_to_merge = []
-    for f in os.listdir(src_dir):
-        if (not os.path.isfile(os.path.join(src_dir, f))): continue
-        if not f.endswith(".json"): continue
-        is_desired_location = f.startswith(prefix)
-        if is_desired_location:
-            files_to_merge.append(os.path.join(src_dir, f))
-    merged = merge_coco_json(files_to_merge)
+
+def merge_all_in_dir(src_dir, dest_path):
+    files_to_merge = [f for f in os.listdir(src_dir) if os.path.isfile(os.path.join(src_dir, f)) and f.endswith(".json")]
+    merged = merge_coco_json(coco_list=files_to_merge)
+    save_merged_annotations(merged=merged, destination_path=dest_path)
     
-    return merged
-
-
 def main():
-    destination_dir = "data-annotations/pitfall-cameras/merged-by-location/"
-    src_dir = "data-annotations/pitfall-cameras/originals-converted/"
-    destination = destination_dir + "GH-OSR-HF2G.json"
-    merged = merge_by_location(field="GH", crop="OSR", camera="HF2G")
-    save_merged_annotations(merged=merged, destination_path=destination)
+    merge_by_location()
+
+
+
 
 
 if __name__ == "__main__":
